e8d0d22037f2470352053b591cbdaefcb8594bbb
lbogus <lbogus@huskers.unl.edu>
2019-04-01 16:39:59 -0500

    CI should fail, since there is a failing unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,12 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Add_Valid()
+        {
+            Assert.AreEqual(4, Program.Add("1", "2"));
+            Assert.AreEqual(5, Program.Add("3", "2"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
+        }
     }
 }
